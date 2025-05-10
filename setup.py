01342324454872a3880dd3516397299e7f094723
Avi Zaguri <49598021+aviz92@users.noreply.github.com>
2025-05-10 19:46:54 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='python-test-aviz',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(include=['python_test_aviz', 'python_test_aviz.*']),
     install_requires=[
         'setuptools>=42.0.0',
