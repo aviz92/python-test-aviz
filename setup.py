9b5cafac0a9092338effaffbc300eef28cbd3df5
Avi Zaguri <49598021+aviz92@users.noreply.github.com>
2025-05-17 13:56:30 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-package_version = '1.0.1'
+package_version = '1.0.2'
 
 package_name = 'python-test-aviz'
 package_description = 'A test repository for testing the publishing process of a Python package.'
