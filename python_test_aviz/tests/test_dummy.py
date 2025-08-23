69855af1ea84738fe2c958787b7dba96da93f0bc
avizaguri-macbook <avi210892@gmail.com>
2025-08-23 21:17:20 +0300

    Add commit status updates for pytest marker workflow

--- python_test_aviz/tests/test_dummy.py
+++ python_test_aviz/tests/test_dummy.py
@@ -1,6 +1,16 @@
 import pytest
 
 
+class TestDummy:
+    @pytest.mark.dummy
+    def test_dummy(self) -> None:
+        assert True
+
+    @pytest.mark.dummy
+    def test_true(self) -> None:
+        assert True
+
+
 @pytest.mark.dummy
 def test_true() -> None:
     assert True
